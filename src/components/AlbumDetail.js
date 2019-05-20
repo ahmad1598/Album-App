16e9b9c1c0f0f9ae374f92b72dbaad741987bdef
Ahmad Rasoulpour <ahmad.raman83@yahoo.com>
2019-05-19 23:39:54 -0600

    Album detail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,68 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, Linking } from 'react-native'
 import Card from './Card';
 import CardSection from './CardSection'
-const AlbumDetail = (props) => {
+import Button from './Button'
+
+// destructuring album from props
+const AlbumDetail = ({album}) => {
+    const {title, artist, thumbnail_image, image, url} = album
+    const {
+        imageStyle,
+        headerContentStyle,
+        imageContainerStyle,
+        headerTextStyle, 
+        coverImageStyle} = styles
     return(
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View style={imageContainerStyle}>
+                    <Image style={imageStyle} source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image style={coverImageStyle} source={{uri: image}} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     )
 }
 
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent:'space-around'
+    },
+    headerTextStyle:{
+        fontSize: 18
+    },
+    imageStyle:{
+        height: 50,
+        width: 50
+    },
+    imageContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    coverImageStyle:{
+        height: 300,
+        flex: 1,
+        width: null
+    }
+
+}
 
 export default AlbumDetail
